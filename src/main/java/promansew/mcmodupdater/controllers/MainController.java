2fc3b6e8878046d2aa2d0b995edc0a3e05d39fed
Roman Parshikov <promansew@gmail.com>
2019-09-14 05:46:19 +0800

    Список модов в окне

--- src/main/java/promansew/mcmodupdater/controllers/MainController.java
+++ src/main/java/promansew/mcmodupdater/controllers/MainController.java
@@ -1,10 +1,31 @@
 package promansew.mcmodupdater.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import promansew.mcmodupdater.MCData;
+import promansew.mcmodupdater.model.Mod;
 
 public class MainController {
 
 	@FXML
+	private TableView<Mod> mods;
+
+	@FXML
+	@SuppressWarnings("unchecked")
 	public void initialize() {
+		ObservableList<TableColumn<Mod, ?>> columns = mods.getColumns();
+		TableColumn<Mod, ?> names = columns.get(0);
+		TableColumn<Mod, ?> versions = columns.get(1);
+		names.setCellValueFactory(new PropertyValueFactory("name"));
+		versions.setCellValueFactory(new PropertyValueFactory("version"));
+		names.prefWidthProperty().bind(mods.widthProperty().multiply(0.6));
+		versions.prefWidthProperty().bind(mods.widthProperty().multiply(0.4));
+		names.setResizable(false);
+		versions.setResizable(false);
+		mods.setItems(FXCollections.observableList(MCData.getMods()));
 	}
 }
--- src/main/java/promansew/mcmodupdater/model/Mod.java
+++ src/main/java/promansew/mcmodupdater/model/Mod.java
@@ -1,16 +1,29 @@
 package promansew.mcmodupdater.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Mod {
-	public final String name;
-	public final String version;
+	private final StringProperty name;
+	private final StringProperty version;
 
 	public Mod(String name, String version) {
-		this.name = name;
-		this.version = version;
+		this.name = new SimpleStringProperty(this, "name");
+		this.name.setValue(name);
+		this.version = new SimpleStringProperty(this, "version");
+		this.version.setValue(version);
+	}
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public StringProperty versionProperty() {
+		return version;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("[name: %s, version: %s]", name, version);
+		return String.format("[name: %s, version: %s]", name.getValue(), version.getValue());
 	}
 }
